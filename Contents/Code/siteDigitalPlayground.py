e9dcbc82a9588ce5d72d7a4b7c4b3cfa982b1227
pahelper <pahelper@sahasrahla.com>
2018-12-17 21:28:25 -0600

    Even more fiddling with DigitalPlayground

--- Contents/Code/siteDigitalPlayground.py
+++ Contents/Code/siteDigitalPlayground.py
@@ -17,6 +17,8 @@ def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor
     searchResults = HTML.ElementFromURL(PAsearchSites.getSearchSearchURL(siteNum) + encodedTitle)
     for searchResult in searchResults.xpath('//div[@class="box-card scene"]'):
         titleNoFormatting = searchResult.xpath('.//img[@class=" lazyload"]')[0].get('alt')
+        #k = titleNoFormatting.rfind("-")
+        #titleNoFormatting = titleNoFormatting[:k].strip()
         Log("Result Title: " + titleNoFormatting)
         curID = PAsearchSites.getSearchBaseURL(siteNum) + searchResult.xpath('.//a[@class="track"]')[0].get('href')
         curID = curID.replace('/','_')
@@ -34,28 +36,35 @@ def update(metadata, siteID, movieGenres):
 
     # Tagline
     try:
-        tagline = detailsPageElements.xpath('//a[@class="full-scene-button "]')[0].text_content().strip()
+        #Typical tagline for Flixxx and Raw Cuts, etc.
+        tagline = detailsPageElements.xpath('//a[contains(@class,"full-scene-button")]')[0].text_content().strip()
     except:
         pass
 
     # Summary
     try:
         #Typical summary for Flixxx and Raw Cuts, etc.
-        summary = detailsPageElements.xpath('//span[text()="SYNOPSIS"]/following::span')[0].text_content().strip()
+        summary = detailsPageElements.xpath('//span[text()="SYNOPSIS"]//following::span')[0].text_content().strip()
     except:
         pass
+
     try:
         #Series keep their summary on the Series Info page
-        seriesPage = detailsPageElements.xpath('//a[text()="info"]')[0].get("href")
+        seriesPage = PAsearchSites.getSearchBaseURL(siteID) + detailsPageElements.xpath('//a[contains(text(),"info")]')[0].get("href")
         seriesPageElements = HTML.ElementFromURL(seriesPage)
-        summary = seriesPageElements.xpath('//div[@class="overview"]/following::p')[0].text_content().strip()
+        summary = seriesPageElements.xpath('//div[@class="overview"]//p')[0].text_content().strip()
         tagline = "Series: " + seriesPageElements.xpath('//h1')[0].text_content().strip()
     except:
         pass
+
     if tagline == "Full Movie":
         tagline = "Blockbuster"
     tagline = "DP " + tagline
+
+    # Title
     title = detailsPageElements.xpath('//h1')[0].text_content().strip()
+    #k = title.rfind(" - ")
+    #title = title[:k].strip()
 
     metadata.collections.clear()
     metadata.collections.add(tagline)
@@ -63,9 +72,12 @@ def update(metadata, siteID, movieGenres):
     metadata.title = title
     metadata.summary = summary
 
+    thisPage = detailsPageElements.xpath('//a[contains(text(),"trailer")]')[0].get('href')
+    Log("thisPage: " + thisPage)
+
     # Genres
     movieGenres.clearGenres()
-    genres = detailsPageElements.xpath('//ul[@id="movie-info-format"]//li//div//a')
+    genres = detailsPageElements.xpath('//ul[@id="movie-info-format" and last()]/li/div/a')
     if len(genres) > 0:
         for genreLink in genres:
             genreName = str(genreLink.text_content().lower())
@@ -80,22 +92,44 @@ def update(metadata, siteID, movieGenres):
 
     # Actors
     metadata.roles.clear()
+
     try:
         # This should work for most things
-        actors = detailsPageElements.xpath('//span[text()="STARRING"]/following::span//a')
+        actors = detailsPageElements.xpath('//span[@class="subtitle" and text()="STARRING"]//following::span[1]//a')
+        Log("Actors by STARRING: " + str(len(actors)))
+        if len(actors) == 0:
+            raise
     except:
-        # Fallback plan is to find the actors on the Search Page results
-        searchPageElements = HTML.ElementFromURL("https://www.digitalplayground.com/search/videos/" + urllib.quote(title))
-        actors = searchPageElements.xpath('//h4[contains(text(),"'+title+'"]/following-sibling::a')
+        try:
+            # Series needs to define the Episode Number and pull only actors from that episode
+            actors = detailsPageElements.xpath('//a[@href="'+thisPage+'" and last()]//following-sibling::div[@class="model-names-wrapper"]/span[@class="model-names"]/a')
+            Log("Actors by Episode: " + str(len(actors)))
+            if len(actors) == 0:
+                raise
+
+        except:
+            try:
+                # Fallback plan is to find the actors on the Search Page results
+                searchPageElements = HTML.ElementFromURL(PAsearchSites.getSearchSearchURL(siteID) + urllib.quote(title))
+                actors = searchPageElements.xpath('//h4[contains(text(),"'+title+'"]/following-sibling::a')
+                Log("Actors by search page: " + str(len(actors)))
+                if len(actors) == 0:
+                    raise
+            except:
+                Log("Didn't find any actors in any method")
+
+    Log("Actor count: " + str(len(actors)))
+
     if len(actors) > 0:
         for actorLink in actors:
-            role = metadata.roles.new()
-            actorName = str(actorLink.text_content().strip())
-            role.name = actorName
             actorPageURL = actorLink.get("href")
-            actorPage = HTML.ElementFromURL(PAsearchSites.getSearchBaseURL(siteID)+actorPageURL)
-            actorPhotoURL = "https:" + actorPage.xpath('//div[@class="preview-image"]//img')[0].get("src")
-            role.photo = actorPhotoURL
+            if "/model/" in actorPageURL: # dirty hack to filter out the extra actor I was getting that was named for some other scene; actual problem is probably just my xpath search for actors above
+                role = metadata.roles.new()
+                actorName = str(actorLink.text_content().strip())
+                role.name = actorName
+                actorPage = HTML.ElementFromURL(PAsearchSites.getSearchBaseURL(siteID)+actorPageURL)
+                actorPhotoURL = "https:" + actorPage.xpath('//div[@class="preview-image"]//img')[0].get("src")
+                role.photo = actorPhotoURL
 
 
     # Posters
