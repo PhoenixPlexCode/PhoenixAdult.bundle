ddc6af785300871c5106203b5416d4f8350169eb
pahelper <pahelper@sahasrahla.com>
2019-01-20 19:20:14 -0600

    Bugfix

--- Contents/Code/PAsearchSites.py
+++ Contents/Code/PAsearchSites.py
@@ -34,7 +34,7 @@ import siteJulesJordan
 import networkDDFNetwork
 import networkPerfectGonzo
 
-searchSites = [None] * 462
+searchSites = [None] * 466
 searchSites[1] = ["Blacked com","Blacked","https://www.blacked.com","https://www.blacked.com/search?q="]
 searchSites[0] = ["Blackedraw com","BlackedRaw","https://www.blackedraw.com","https://www.blackedraw.com/search?q="]
 searchSites[2] = ["Brazzers.com","Brazzers","http://www.brazzers.com","http://www.brazzers.com/search/all/?q="]
