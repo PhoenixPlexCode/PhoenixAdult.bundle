fce5aa85feb700c718f9c380f4201dcf576095e6
SgtBatten <sgt@batten.net.au>
2019-02-28 17:53:43 +1100

    Improvements to pornpros.
    Better summaries for passionHD and spyfam.
    
    Still some titles which I can reliably reproduce to be unsuccessful in matching to the fan site despite no obvious reason why.

--- Contents/Code/networkPornPros.py
+++ Contents/Code/networkPornPros.py
@@ -102,8 +102,7 @@ def update(metadata,siteID,movieGenres,movieActors):
         urls = search('"AnalPornfan "' + actorName + ' ' + metadata.title , stop=2)
     elif siteName.lower() == "SpyFam".lower():
         urls = search('site:SpyFams.com ' + actorName + ' ' + metadata.title , stop=2)
-    
- 
+
     match = 0
     for url in urls:
         if match is 0:
@@ -115,34 +114,46 @@ def update(metadata,siteID,movieGenres,movieActors):
                     if siteName.lower() == "Holed".lower():
                         nameinheader = fanPageElements.xpath('//div[@class="page-title pad group"]//a[2]')[0].text_content()
                     elif siteName.lower() == "SpyFam".lower():
-                        nameinheader = fanPageElements.xpath('//span[@itemprop="articleSection"]')[0].text_content()
+                        nameinheader = fanPageElements.xpath('(//span[@itemprop="articleSection"])')[0].text_content()
                     else:
                         nameinheader = fanPageElements.xpath('//div[@class="page-title pad group"]//a')[0].text_content()
                     Log("Actress name in header: " + nameinheader)
                 except:
                     pass
-
+                    
                 if actorName in nameinheader:
                     Log(siteName + " Fansite Match Found")
                     match = 1
+                else:
+                    # When there are multiple actors listed we need to check all of them.
+                    try:
+                        for actorLink in actors:
+                            actorName = actorLink.text_content()
+                            for name in nameinheader:
+                                if actorName.lower() == name.lower():
+                                    Log(siteName + " Fansite Match Found")
+                                    match = 1
+                    except:
+                        pass
+                        
+                if match is 1:
                     # Summary
                     if siteName.lower() == "SpyFam".lower():
                         paragraphs = fanPageElements.xpath('(//div[@class="entry-content g1-typography-xl"]//p)[not(*[contains(@class, "jp-relatedposts-post")])]')
-                        Log(len(paragraphs))
                         if len(paragraphs) > 3:
                             pNum = 1
                             summary = ""
                             for paragraph in paragraphs:
-                                if pNum >= 1 and pNum <= 5:
-                                    summary = summary + '\n' + paragraph.text_content()
+                                if pNum >= 1 and pNum <= 7:
+                                    summary = summary + '\n\n' + paragraph.text_content()
                                 pNum += 1
-
-                            metadata.summary = summary.strip()  
+                            metadata.summary = summary.strip()
                         else:
                             metadata.summary = fanPageElements.xpath('(//div[@class="entry-content g1-typography-xl"]//p)[position()=1]')[0].text_content().strip()
                     else:
                         metadata.summary = fanPageElements.xpath('//div[@class="entry-inner"]//p')[0].text_content().replace("---->Click Here to Download<----", '').strip()
-
+                    
+                     
                 # Various Poster xpaths needed for different sites
                 try:
                     if siteName.lower() == "Lubed".lower():
@@ -170,6 +181,7 @@ def update(metadata,siteID,movieGenres,movieActors):
         # If you want more or less posters edít the value in random.sample below or refresh metadata to get a different sample.	
         sample = [art[0], art[-1]] + random.sample(art, 4)     
         art = sample
+        Log("Selecting first, last and random 4 images from set")
 
         j = 1
 											  
@@ -195,3 +207,7 @@ def update(metadata,siteID,movieGenres,movieActors):
                     pass
 
     return metadata
+
+
+
+
