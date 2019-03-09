559a234f775ae8f7661adc8f2957b0addf13b841
Mont <malkman71@gmail.com>
2019-03-09 17:35:46 -0500

    added scene title url match to badoinkvr sites as a fallback

--- Contents/Code/networkBadoinkVR.py
+++ Contents/Code/networkBadoinkVR.py
@@ -1,63 +1,62 @@
 import PAsearchSites
 import PAgenres
 def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor,searchDate,searchSiteID):
-    # try:
-        # Actress Search
-    url = PAsearchSites.getSearchSearchURL(searchSiteID) + searchTitle.lower().replace(" ","")
-    Log('Actress Page: ' + url)
-    searchResults = HTML.ElementFromURL(url)
-    Log('Actress Search')
-    for searchResult in searchResults.xpath('//section[1]//div[@class="tile-grid-item"]'):
-        titleNoFormatting = searchResult.xpath('.//a[@class="video-card-title heading heading--4"]')[0].get('title')
-        Log("Result Title: " + titleNoFormatting)
-        curID = searchResult.xpath('.//a[@class="video-card-title heading heading--4"]')[0].get('href')
-        curID = curID.replace("_","-").replace("/","_")
+    # try Actress Search
+    try:
+        url = PAsearchSites.getSearchSearchURL(searchSiteID) + searchTitle.lower().replace(" ","")
+        Log('Actress Page: ' + url)
+        searchResults = HTML.ElementFromURL(url)
+        Log('Actress Search')
+        for searchResult in searchResults.xpath('//section[1]//div[@class="tile-grid-item"]'):
+            titleNoFormatting = searchResult.xpath('.//a[@class="video-card-title heading heading--4"]')[0].get('title')
+            Log("Result Title: " + titleNoFormatting)
+            curID = searchResult.xpath('.//a[@class="video-card-title heading heading--4"]')[0].get('href')
+            curID = curID.replace("_","+").replace("/","_")
+            Log("curID: " + curID)
+            releaseDate = parse(searchResult.xpath('.//span[@class="video-card-upload-date"]')[0].text_content().strip()).strftime('%Y-%m-%d')
+            Log("releaseDate: " + releaseDate)
+            girlName = searchResult.xpath('.//a[@itemprop="actor"]')[0].text_content().strip()
+            if searchDate:
+                score = 100 - Util.LevenshteinDistance(searchDate, releaseDate)
+            else:
+                score = 100 - Util.LevenshteinDistance(searchTitle.lower(), girlName.lower())
+
+            results.Append(MetadataSearchResult(id = curID + "|" + str(searchSiteID), name = girlName + " in " + titleNoFormatting + " [" + PAsearchSites.getSearchSiteName(searchSiteID) + "] " + releaseDate, score = score, lang = lang))
+    # revert to scene title search
+    except:
+        Log("Scene Title Search")
+        siteName = PAsearchSites.getSearchFilter(searchSiteID)
+        if siteName == "KinkVR":
+            scenePageBase = "/bdsm-vr-video/"
+        elif siteName == "VRCosplayX":
+            scenePageBase = "/cosplaypornvideo/"
+        else:
+            scenePageBase = "/vrpornvideo/"
+        Log('scenePageBase: ' + scenePageBase)
+        searchString = searchTitle.lower().replace(" ","_").replace("'","_").replace(".","_").replace(",","")
+        url = PAsearchSites.getSearchBaseURL(searchSiteID) + scenePageBase + searchString
+        Log("Scene Page url: " + url)
+        searchResults = HTML.ElementFromURL(url)
+        searchResult = searchResults.xpath('//div[@class="video-rating-and-details"]')[0]
+        titleNoFormatting = searchResult.xpath('.//h1[@class="heading heading--2 video-title"]')[0].text_content()
+        Log("SceneTitle: " + titleNoFormatting)
+        curID = searchResults.xpath('//link[@rel="canonical"]')[0].get('href')
+        curID = curID.replace("_","+").replace("/","_")
         Log("curID: " + curID)
-        releaseDate = parse(searchResult.xpath('.//span[@class="video-card-upload-date"]')[0].text_content().strip()).strftime('%Y-%m-%d')
-        Log("releaseDate: " + releaseDate)
-        girlName = searchResult.xpath('.//a[@itemprop="actor"]')[0].text_content().strip()
+        releaseDate = parse(searchResult.xpath('.//div[@class="video-details"]//p[@class="video-upload-date"]')[0].get('content').strip()).strftime('%Y-%m-%d')
+        Log("ReleaseDate: " + releaseDate)
+        girlName = searchResult.xpath('.//a[contains(@class,"video-actor-link")]')[0].text_content()
+        Log("firstActressName: " + girlName)
         if searchDate:
             score = 100 - Util.LevenshteinDistance(searchDate, releaseDate)
         else:
-            score = 100 - Util.LevenshteinDistance(searchTitle.lower(), girlName.lower())
-
+            score = 100 - Util.LevenshteinDistance(searchTitle.lower(), titleNoFormatting.lower())
         results.Append(MetadataSearchResult(id = curID + "|" + str(searchSiteID), name = girlName + " in " + titleNoFormatting + " [" + PAsearchSites.getSearchSiteName(searchSiteID) + "] " + releaseDate, score = score, lang = lang))
-
-    # except:
-    #     # Scene title search
-    #     Log("Scene Title Search")
-    #     siteName = PAsearchSites.getSearchFilter(searchSiteID)
-    #     if siteName == "KinkVR":
-    #         scenePageBase = "/bdsm-vr-video/"
-    #     elif siteName == "VRCosplayX":
-    #         scenePageBase = "/cosplaypornvideo/"
-    #     else:
-    #         scenePageBase = "/vrpornvideo/"
-    #
-    #     url = PAsearchSites.getSearchBaseURL(searchSiteID) + scenePageBase + searchTitle.lower().replace(" ","_").replace("'","_")
-    #     Log("Scene Page url: " + url)
-    #     searchResults = HTML.ElementFromURL(url)
-    #     searchResult = searchResults.xpath('//div[@class="video-rating-and-details"]')[0]
-    #     titleNoFormatting = searchResult.xpath('.//h1[@class="heading heading--2 video-title"]')[0].text_content()
-    #     Log("SceneTitle: " + titleNoFormatting)
-    #     curID = scenePageBase + searchTitle.lower().replace(" ","_").replace("'","_")
-    #     curID = curID.replace("/","?")
-    #     Log("curID: " + curID)
-    #
-    #     releaseDate = parse(searchResult.xpath('.//div[@class="video-details"]//p[@class="video-upload-date"]')[0].get('content').strip()).strftime('%Y-%m-%d')
-    #     Log("ReleaseDate: " + releaseDate)
-    #     girlName = searchResult.xpath('.//a[contains(@class,"video-actor-link")]')[0].text_content()
-    #     Log("firstActressName: " + girlName)
-    #     if searchDate:
-    #         score = 100 - Util.LevenshteinDistance(searchDate, releaseDate)
-    #     else:
-    #         score = 100 - Util.LevenshteinDistance(searchTitle.lower(), titleNoFormatting.lower())
-    #     results.Append(MetadataSearchResult(id = curID + "|" + str(searchSiteID), name = girlName + " in " + titleNoFormatting + " [" + PAsearchSites.getSearchSiteName(searchSiteID) + "] " + releaseDate, score = score, lang = lang))
 
     return results
 
 def update(metadata,siteID,movieGenres,movieActors):
-    temp = str(metadata.id).split("|")[0].replace("_","/").replace("-","_").replace('_','-',-1)
+    temp = str(metadata.id).split("|")[0].replace("_","/").replace("+","_")
     url = PAsearchSites.getSearchBaseURL(siteID) + temp
     detailsPageElements = HTML.ElementFromURL(url)
 
