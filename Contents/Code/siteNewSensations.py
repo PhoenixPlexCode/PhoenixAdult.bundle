35ae4661598b7c39ccd334166a1a7dd22dc39790
Mont <malkman71@gmail.com>
2019-03-11 02:07:01 -0400

    changed newsensations file to actress match because public search no longer availible (any search redirects to main page and gives whatever results featured there) anonymized links in readme

--- Contents/Code/siteNewSensations.py
+++ Contents/Code/siteNewSensations.py
@@ -2,41 +2,68 @@ import PAsearchSites
 import PAgenres
 
 def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor,searchDate, searchSiteID):
-    searchResults = HTML.ElementFromURL(PAsearchSites.getSearchSearchURL(siteNum) + encodedTitle)
-    for searchResult in searchResults.xpath('//h4//a'):
-        Log(str(searchResult.get('href')))
-        titleNoFormatting = searchResult.text_content()
-        curID = searchResult.get('href').replace('/','_').replace('?','!')
-        score = 100 - Util.LevenshteinDistance(title.lower(), titleNoFormatting.lower())
-        
-        results.Append(MetadataSearchResult(id = curID + "|" + str(siteNum), name = titleNoFormatting + " [New Sensations]", score = score, lang = lang))
+    if searchSiteID != 9999:
+        siteNum = searchSiteID
+    try:
+        # actress search
+        try:
+            searchString = searchTitle.replace(" ","-")
+            searchResults = HTML.ElementFromURL(PAsearchSites.getSearchBaseURL(searchSiteID) + "/tour_ns/models/" + searchString + ".html")
+        except:
+            searchString = searchTitle.replace(" ","")
+            searchResults = HTML.ElementFromURL(PAsearchSites.getSearchBaseURL(searchSiteID) + "/tour_ns/models/" + searchString + ".html")
+        for searchResult in searchResults.xpath('//div[contains(@class,"videoBlock")]'):
+            titleNoFormatting = searchResult.xpath('.//div[contains(@class,"caption")]//h4//a')[0].text_content()
+            Log("titleNoFormatting: " + titleNoFormatting)
+            curID = searchResult.xpath('.//div[contains(@class,"caption")]//h4//a')[0].get('href').replace('/', '_').replace('?', '!')
+            Log("curID: " + curID)
+            releaseDate = parse(searchResult.xpath('.//p[2]')[0].text_content()[8:20].strip()).strftime('%Y-%m-%d')
+            Log("releaseDate: " + releaseDate)
+            if searchDate:
+                score = 100 - Util.LevenshteinDistance(searchDate, releaseDate)
+            else:
+                score = 100 - Util.LevenshteinDistance(searchTitle.lower(), titleNoFormatting.lower())
+            results.Append(MetadataSearchResult(id = curID + "|" + str(siteNum), name = titleNoFormatting + " [New Sensations]", score = score, lang = lang))
+
+    except:
+        # search by DVD
+            searchString = searchTitle.lower().replace(" ","-").replace("#","").replace("'","").replace("vol. ","").replace("volume ","")
+            searchResults = HTML.ElementFromURL(PAsearchSites.getSearchBaseURL(searchSiteID) + "/tour_ns/dvds/" + searchString + ".html")
+            for searchResult in searchResults.xpath('//div[@class="dvdScene"]'):
+                titleNoFormatting = searchResult.xpath('.//h4//a')[0].text_content()
+                Log("titleNoFormatting: " + titleNoFormatting)
+                curID = searchResult.xpath('.//h4//a')[0].get('href').replace('/','_').replace('?','!')
+                Log("curID: " + curID)
+                releaseDate = parse(searchResult.xpath('.//div[@class="date"]')[0].text_content().split(' l ')[0].replace("Released","").strip()).strftime('%Y-%m-%d')
+                Log("releaseDate: " + releaseDate)
+                if searchDate:
+                    score = 100 - Util.LevenshteinDistance(searchDate, releaseDate)
+                else:
+                    score = 100 - Util.LevenshteinDistance(searchTitle.lower(), titleNoFormatting.lower())
+                results.Append(MetadataSearchResult(id = curID + "|" + str(siteNum), name = titleNoFormatting + " [New Sensations]", score = score, lang = lang))
+
+# From when site had search functionality, don't want to delete it if it comes back
+    # searchResults = HTML.ElementFromURL(PAsearchSites.getSearchSearchURL(siteNum) + encodedTitle)
+    # for searchResult in searchResults.xpath('//h4//a'):
+    #     Log(str(searchResult.get('href')))
+    #     titleNoFormatting = searchResult.text_content()
+    #     curID = searchResult.get('href').replace('/','_').replace('?','!')
+    #     score = 100 - Util.LevenshteinDistance(title.lower(), titleNoFormatting.lower())
+    #
+    #     results.Append(MetadataSearchResult(id = curID + "|" + str(siteNum), name = titleNoFormatting + " [New Sensations]", score = score, lang = lang))
     return results
 
 def update(metadata,siteID,movieGenres,movieActors):
     Log('******UPDATE CALLED*******')
     metadata.studio = 'New Sensations'
+    # Scene(details) page
     detailsPageElements = HTML.ElementFromURL(str(metadata.id).split("|")[0].replace('_','/').replace('!','?').replace('tour/ns','tour_ns'))
 
-    # Summary
-    paragraph = detailsPageElements.xpath('//div[@class="trailerInfo"]//p')[0].text_content().strip()
-    metadata.summary = paragraph
-    tagline = detailsPageElements.xpath('//div[@class="trailerInfo"]//ul//li[4]')[0].text_content().strip()
-    metadata.collections.clear()
-    tagline = tagline.strip()
-    metadata.tagline = tagline
-    #metadata.collections.add(tagline)
+    # Scene Title
     metadata.title = detailsPageElements.xpath('//div[@class="trailerVideos clear"]//div[@class="title clear"]//h3')[0].text_content()
 
-    # Genres
-    movieGenres.clearGenres()
-    genres = detailsPageElements.xpath('//div[@class="trailerInfo"]//ul//li[3]//a')
-
-    if len(genres) > 0:
-        for genreLink in genres:
-            genreName = genreLink.text_content().strip('\n').lower()
-            Log("Genre Found: "+genreName)
-            movieGenres.addGenre(genreName)
-
+    # Summary
+    metadata.summary = detailsPageElements.xpath('//div[@class="trailerInfo"]//p')[0].text_content().strip()
 
     # Release Date
     date = detailsPageElements.xpath('//div[@class="trailerInfo"]//ul//li[2]')
@@ -46,6 +73,54 @@ def update(metadata,siteID,movieGenres,movieActors):
         date_object = datetime.strptime(date, '%m/%d/%Y')
         metadata.originally_available_at = date_object
         metadata.year = metadata.originally_available_at.year
+
+    # DVD name
+    tagline = detailsPageElements.xpath('//div[@class="trailerInfo"]//ul//li[4]//a')[0].text_content().strip()
+    Log("DVD name/tagline: " + tagline)
+    metadata.collections.clear()
+    metadata.tagline = tagline
+    metadata.collections.add(tagline)
+
+    # DVD Page
+    dvdPageLink = detailsPageElements.xpath('//div[@class="trailerInfo"]//ul//li[4]//a')[0].get("href")
+    Log("dvdPageLink: " + dvdPageLink)
+    dvdPageElements = HTML.ElementFromURL(dvdPageLink)
+
+    #
+    valid_names = list()
+    metadata.posters.validate_keys(valid_names)
+    metadata.art.validate_keys(valid_names)
+
+    # DVD Cover as first poster
+    posterNum = 1
+    dvdPosterURL = dvdPageElements.xpath('//div[@class="dvdcover"]//img')[0].get("src")
+    metadata.posters[dvdPosterURL] = Proxy.Preview(HTTP.Request(dvdPosterURL, headers={'Referer': 'http://www.google.com'}).content, sort_order = posterNum)
+    Log("added dvd poster: " + dvdPosterURL)
+    posterNum += 1
+
+
+    # Background
+    bgNum = 1
+    try:
+        # background on details page is first
+        background = detailsPageElements.xpath('//span[@id="trailer_thumb"]//img')[0].get('src')
+        Log("BG DL: " + background)
+        metadata.art[background] = Proxy.Preview(HTTP.Request(background, headers={'Referer': 'http://www.google.com'}).content, sort_order = bgNum)
+        bgNum += 1
+        # also possible poster
+        metadata.posters[background] = Proxy.Preview(HTTP.Request(background, headers={'Referer': 'http://www.google.com'}).content, sort_order = posterNum)
+        posterNum +=1
+    except:
+        pass
+
+    # Genres
+    movieGenres.clearGenres()
+    genres = detailsPageElements.xpath('//div[@class="trailerInfo"]//ul//li[3]//a')
+    if len(genres) > 0:
+        for genreLink in genres:
+            genreName = genreLink.text_content().strip('\n').lower()
+            Log("Genre Found: "+genreName)
+            movieGenres.addGenre(genreName)
 
     # Actors
     movieActors.clearActors()
@@ -58,14 +133,16 @@ def update(metadata,siteID,movieGenres,movieActors):
             actorPage = HTML.ElementFromURL(actorPageURL)
             actorPhotoURL = actorPage.xpath('//div[@class="modelPicture"]//img')[0].get("src")
             movieActors.addActor(actorName,actorPhotoURL)
+            # add actress image as possible poster
+            if actors < 3:
+                metadata.posters[actorPhotoURL] = Proxy.Preview(HTTP.Request(actorPhotoURL, headers={'Referer': 'http://www.google.com'}).content, sort_order = posterNum)
+                posterNum += 1
+    # DVD page only place with more thumbs
+    for dvdThumb in dvdPageElements.xpath('//div[@class="dvdScenePic"]//img'):
+        dvdThumbURL = dvdThumb.get("src")
+        metadata.art[dvdThumbURL] = Proxy.Preview(HTTP.Request(dvdThumbURL, headers={'Referer': 'http://www.google.com'}).content, sort_order = bgNum)
+        Log("added dvd scene background: " + dvdThumbURL)
+        bgNum += 1
 
-    #Posters
-    #i = 1
-    #try:
-    #    background = "http:" + detailsPageElements.xpath('//*[@class="player-video"]/img')[0].get('src')
-    #    Log("BG DL: " + background)
-    #    metadata.art[background] = Proxy.Preview(HTTP.Request(background, headers={'Referer': 'http://www.google.com'}).content, sort_order = 1)
-    #except:
-    #    pass
 
     return metadata
