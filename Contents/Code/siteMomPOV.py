1ca108769b497e95e990868f9e4c7edb665e21e7
claygoldfinch <49423016+claygoldfinch@users.noreply.github.com>
2019-06-14 20:02:32 -0400

    MomPOV Bugfixes/Improvements

--- Contents/Code/siteMomPOV.py
+++ Contents/Code/siteMomPOV.py
@@ -6,11 +6,11 @@ def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor
     if searchSiteID != 9999:
         siteNum = searchSiteID
     searchResults = HTML.ElementFromURL(PAsearchSites.getSearchSearchURL(siteNum) + encodedTitle)
-    for searchResult in searchResults.xpath('//div[@id="inner_content"]/div'):
+    for searchResult in searchResults.xpath('//div[@id="inner_content"]/div[@class="entry"]'):
         titleNoFormatting = searchResult.xpath('.//div[@class="title_holder"]/h1/a')[0].text_content().strip()
         curID = searchResult.xpath('.//div[@class="title_holder"]/h1/a')[0].get('href').replace('/','_').replace('?','!')
         day = searchResult.xpath('.//div[@class="date_holder"]/span[2]')[0].text_content().strip()
-        month = searchResult.xpath('.//div[@class="date_holder"]/span[1]')[0].text_content().replace('2019', '').replace('2018', '').replace('2017', '').replace('2016', '').replace('2015', '').replace('2014', '').replace('2013', '').strip()
+        month = searchResult.xpath('.//div[@class="date_holder"]/span[1]')[0].text_content()[:3].strip()
         year = searchResult.xpath('.//div[@class="date_holder"]/span[1]/span')[0].text_content().strip()
         date = (month + " " + day + " " + year)
         releaseDate = (parse(date)).strftime('%Y-%m-%d')
