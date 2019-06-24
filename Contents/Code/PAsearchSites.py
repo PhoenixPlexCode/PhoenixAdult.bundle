093a6d94f5a3127bb8ffdc3b97e33f1fd142732c
claygoldfinch <49423016+claygoldfinch@users.noreply.github.com>
2019-06-24 16:20:06 -0400

    Preliminary Straplezz Support

--- Contents/Code/PAsearchSites.py
+++ Contents/Code/PAsearchSites.py
@@ -68,8 +68,9 @@ import siteMomPOV
 import sitePropertySex
 import siteFuckedHard18
 import siteTransAngels
+import siteStraplezz
 
-searchSites = [None] * 741
+searchSites = [None] * 742
 searchSites[1] = ["Blacked com","Blacked","https://www.blacked.com","https://www.blacked.com/search?q="]
 searchSites[0] = ["Blackedraw com","BlackedRaw","https://www.blackedraw.com","https://www.blackedraw.com/search?q="]
 searchSites[2] = ["Brazzers.com","Brazzers","http://www.brazzers.com","http://www.brazzers.com/search/all/?q="]
@@ -811,6 +812,7 @@ searchSites[737] = ["TransAngels","TransAngels","https://www.transangels.com","h
 searchSites[738] = ["Girls Gone Pink","Girls Gone Pink","https://www.mofos.com","https://www.mofos.com/scene/"]
 searchSites[739] = ["Real Slut Party","Real Slut Party","https://www.mofos.com","https://www.mofos.com/scene/"]
 searchSites[740] = ["Mofos Lab","Mofos Lab","https://www.mofos.com","https://www.mofos.com/scene/"]
+searchSites[741] = ["Straplezz","Straplezz","https://straplezz.com","https://straplezz.com/updates/"]
 
 def getSearchBaseURL(siteID):
     return searchSites[siteID][2]
