3fa4c15863b218f2f6b8b323315948fa88247137
PAhelper <45471033+PAhelper@users.noreply.github.com>
2019-06-28 22:16:33 -0500

    Update networkGammaEnt.py

--- Contents/Code/networkGammaEnt.py
+++ Contents/Code/networkGammaEnt.py
@@ -25,7 +25,7 @@ def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor
     elif siteNum == 329 or (siteNum >= 351 and siteNum <= 354):
         network = 'Blowpass'
         networkdvd = False
-    elif siteNum == 331 or (siteNum >= 355 and siteNum <= 360) or siteNum == 743:
+    elif siteNum == 331 or (siteNum >= 355 and siteNum <= 360) or siteNum == 750:
         network = 'Fantasy Massage'
         networkdvd = False
         network_sep_scene = "/scene"
