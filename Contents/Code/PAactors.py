8e1e7b24b300f6f064c2e5ab8e318eb6f4276c6b
jamesalexw <james@wirefired.co.uk>
2019-10-02 21:12:07 +0100

    Update PAactors.py (#423)
    Added fix for Wowgirls to correct Clover to Katya Clover - Was being merged with male actor Clover

--- Contents/Code/PAactors.py
+++ Contents/Code/PAactors.py
@@ -2118,6 +2118,9 @@ class PhoenixActors:
             if metadata.studio == "Reality Kings":
                 if newActor == "Morgan":
                     newActor = "Morgan Layne"
+            if metadata.studio == "WowGirls":
+				if newActor == "Clover":
+		    		newActor = "Katya Clover"
 
             if not skip:
                 if newPhoto == '':
