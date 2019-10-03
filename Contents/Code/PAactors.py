c05408fb2ed57cf74d536053d240757b60965341
jamesalexw <james@wirefired.co.uk>
2019-10-03 11:45:52 +0100

    Update PAactors.py

--- Contents/Code/PAactors.py
+++ Contents/Code/PAactors.py
@@ -2118,7 +2118,7 @@ class PhoenixActors:
                     newActor = "Morgan Layne"
             if metadata.studio == "WowGirls":
 				if newActor == "Clover":
-		    		newActor = "Katya Clover"
+					newActor = "Katya Clover"
 
             if not skip:
                 if newPhoto == '':
