d6f99eaaf198750e7170c9dc8e41528bd8751c39
jamesalexw <james@wirefired.co.uk>
2019-10-03 11:48:28 +0100

    Update PAactors.py
    Fix issue with line indents on 2122 & 2123

--- Contents/Code/PAactors.py
+++ Contents/Code/PAactors.py
@@ -2117,8 +2117,8 @@ class PhoenixActors:
                 if newActor == "Morgan":
                     newActor = "Morgan Layne"
             if metadata.studio == "WowGirls":
-				if newActor == "Clover":
-					newActor = "Katya Clover"
+                if newActor == "Clover":
+                    newActor = "Katya Clover"
 
             if not skip:
                 if newPhoto == '':
