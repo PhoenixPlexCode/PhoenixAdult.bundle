2878f75350412f862489807d078accb1c08191ed
delposibl <46036854+delposibl@users.noreply.github.com>
2019-11-05 22:34:32 -0500

    vr bangers full bugfix

--- Contents/Code/siteVRBangers.py
+++ Contents/Code/siteVRBangers.py
@@ -6,17 +6,17 @@ def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor
     searchResults = HTML.ElementFromURL(PAsearchSites.getSearchSearchURL(siteNum) + encodedTitle)
     for searchResult in searchResults.xpath('//article'):
         titleNoFormatting = searchResult.xpath('.//a[@rel="bookmark"]')[0].text_content().strip()
-        detailsPageElements = HTML.ElementFromURL(searchResult.xpath('.//a[@rel="bookmark"]')[0].get('href'))
-        releaseDate = parse(detailsPageElements.xpath('//p[@class="pull-right dates invisible"]')[0].text_content().strip()).strftime('%Y-%m-%d')
-        curID = searchResult.xpath('.//a[@rel="bookmark"]')[0].get('href').replace('/','_').replace('?','!')
-        if searchDate:
-            score = 100 - Util.LevenshteinDistance(searchDate, releaseDate)
-        else:
-            score = 100 - Util.LevenshteinDistance(searchTitle.lower(), titleNoFormatting.lower())
+        Log("Title: " + titleNoFormatting)
+        curID = searchResult.xpath('.//a')[0].get('href').replace('/','_').replace('?','!')
+        Log("curID: " + curID)
+        # Removing date search (not on results page) to speed search and cut down page openings (as it was it opened each search result to check date)
+        # detailsPageElements = HTML.ElementFromURL(curID.replace('_','/').replace('!','?'))
+        # releaseDate = parse(detailsPageElements.xpath('//div[@class="video-content__download-info"]//div[@class="section__item-title-download-space"][2]')[0].text_content().replace("Release date:","").strip()).strftime('%Y-%m-%d')
+        # Log("Release date: " + releaseDate)
+        score = 100 - Util.LevenshteinDistance(searchTitle.lower(), titleNoFormatting.lower())
         if len(titleNoFormatting) > 29:
             titleNoFormatting = titleNoFormatting[:32] + "..."
-
-        results.Append(MetadataSearchResult(id = curID + "|" + str(siteNum), name = titleNoFormatting + " ["+PAsearchSites.getSearchSiteName(siteNum)+"] " + releaseDate, score = score, lang = lang))
+        results.Append(MetadataSearchResult(id = curID + "|" + str(siteNum), name = titleNoFormatting + " ["+PAsearchSites.getSearchSiteName(siteNum)+"] ", score = score, lang = lang))
     return results
 
 def update(metadata,siteID,movieGenres,movieActors):
@@ -28,10 +28,10 @@ def update(metadata,siteID,movieGenres,movieActors):
     metadata.studio = "VR Bangers"
 
     # Title
-    metadata.title = detailsPageElements.xpath('//div[@class="video-info-title"]//h1[@class="pull-left page-title"]//span')[0].text_content()
+    metadata.title = detailsPageElements.xpath('//h1')[0].text_content()
 
     # Summary
-    metadata.summary = detailsPageElements.xpath('//div[@class="mainContent"]/p')[0].text_content().strip()
+    metadata.summary = detailsPageElements.xpath('//div[@class="less-text"]/p[2]')[0].text_content().strip()
 
     # Tagline and Collection
     tagline = "VR Bangers"
@@ -40,37 +40,50 @@ def update(metadata,siteID,movieGenres,movieActors):
     metadata.collections.add(tagline)
 
     # Release Date
-    date = detailsPageElements.xpath('//p[@class="pull-right dates invisible"]')[0].text_content()
+    date = detailsPageElements.xpath('//div[@class="video-content__download-info"]//div[@class="section__item-title-download-space"][2]')[0].text_content().replace("Release date:","").strip()
     date_object = parse(date)
     metadata.originally_available_at = date_object
     metadata.year = metadata.originally_available_at.year
 
     # Genres
     movieGenres.clearGenres()
-    genres = detailsPageElements.xpath('//div[@class="video-tags"]//a[@class="tags-item"]')
+    genres = detailsPageElements.xpath('//div[contains(@class,"video-item-info-tags")]//a')
     if len(genres) > 0:
         for genre in genres:
             movieGenres.addGenre(genre.text_content())
 
     # Actors
     movieActors.clearActors()
-    actors = detailsPageElements.xpath('//div[@class="girls-name"]//div[@class="girls-name-video-space"]//a')
+    actors = detailsPageElements.xpath('//div[@class="video-content__download-info"]//div[contains(@class,"video-item-info--starring")]//a')
     if len(actors) > 0:
         for actorLink in actors:
-            actorName = actorLink.text_content().replace(", ","")
+            actorName = actorLink.text_content().strip()
+            Log("actor: " + actorName)
             actorPageURL = actorLink.get("href")
             actorPage = HTML.ElementFromURL(actorPageURL)
             actorPhotoURL = actorPage.xpath('//div[@class="single-model-featured"]//img')[0].get("src")
             movieActors.addActor(actorName,actorPhotoURL)
 
     # Posters/Background
-    for posterLink in detailsPageElements.xpath('//div[@class="single-video-gallery-free"]/a'):
+    for posterLink in detailsPageElements.xpath('//div[contains(@class,"gallery-top")]/a'):
         art.append(posterLink.get('href'))
+        Log("poster: " + posterLink.get('href'))
 
-    j = 1
+    ## Banner
+    banner = detailsPageElements.xpath('//section[@class="banner"]//img')[0].get("src")
+    Log("banner: " + banner)
+    art.append(banner)
+
+    ## Preview image
+    # preview = detailsPageElements.xpath('//div[@class="video-content__watch"]//img')[0].get("src")
+    # Log("preview img:" + preview)
+    # metadata.art[preview] = Proxy.Preview(HTTP.Request(preview, headers={'Referer': 'http://www.google.com'}).content, sort_order = 1)
+
+
+    j = 2
     Log("Artwork found: " + str(len(art)))
     for posterUrl in art:
-        if not PAsearchSites.posterAlreadyExists(posterUrl,metadata):            
+        if not PAsearchSites.posterAlreadyExists(posterUrl,metadata):
             #Download image file for analysis
             try:
                 img_file = urllib.urlopen(posterUrl)
