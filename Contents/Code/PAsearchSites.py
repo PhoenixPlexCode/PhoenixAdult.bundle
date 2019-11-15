11488f378300dcabd6f0f18a85eef3124c1c7395
jamesalexw <james@wirefired.co.uk>
2019-11-15 00:33:18 +0000

    Added 18OnlyGirls support
    This is a preliminary add but it does work. Can someone with more coding experience tidy it up please?

--- Contents/Code/PAsearchSites.py
+++ Contents/Code/PAsearchSites.py
@@ -93,8 +93,9 @@ import networkIntersec
 import networkMileHighMedia
 import networkCherryPimps
 import siteWicked
+import site18OnlyGirls
 
-searchSites = [None] * 794
+searchSites = [None] * 795
 searchSites[0] = ["Blackedraw com","BlackedRaw","https://www.blackedraw.com","https://www.blackedraw.com/search?q="]
 searchSites[1] = ["Blacked com","Blacked","https://www.blacked.com","https://www.blacked.com/search?q="]
 searchSites[2] = ["Brazzers.com","Brazzers","http://www.brazzers.com","http://www.brazzers.com/search/all/?q="]
@@ -889,7 +890,7 @@ searchSites[790] = ["BCM","BCM.XXX","https://www.pimp.xxx","https://pimp.xxx/sea
 searchSites[791] = ["Petite","Petite.XXX","https://www.pimp.xxx","https://pimp.xxx/search.php?query="]
 searchSites[792] = ["Family","Family.XXX","https://www.pimp.xxx","https://pimp.xxx/search.php?query="]
 searchSites[793] = ["Wicked","Wicked Pictures","https://www.wicked.com","https://wicked.com/en/movie/"]
-
+searchSites[794] = ["18OnlyGirls","18 Only Girls","http://www.18onlygirls.tv","http://18onlygirls.tv/?s="]
 
 def getSearchBaseURL(siteID):
     return searchSites[siteID][2]
