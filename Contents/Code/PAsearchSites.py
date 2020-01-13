9859d9528537d8f8370f734e59eacabd507cc64f
DirtyRacer1337 <DirtyRacer@ya.ru>
2020-01-13 09:21:32 +0700

    Fix site search

--- Contents/Code/PAsearchSites.py
+++ Contents/Code/PAsearchSites.py
@@ -956,9 +956,13 @@ def getSearchSiteIDByFilter(searchFilter):
         #  Blacked -> BlackedRaw
         #  Babes -> FootsieBabes
         #  PassionHD Love Ties -> HD Love
-        if sites[0].lower().replace(" ","").replace("'","") in searchFilter.lower().replace(".com","").replace("'","") or sites[0].lower().replace(" ","").replace("'","") in searchFilter.lower().replace(".com","").replace(" ","").replace("'",""):
-            return searchID
+        try:
+            if sites[0].lower().replace(" ","").replace("'","") in searchFilter.lower().replace(".com","").replace("'","") or sites[0].lower().replace(" ","").replace("'","") in searchFilter.lower().replace(".com","").replace(" ","").replace("'",""):
+                return searchID
+        except:
+            pass
         searchID += 1
+
     return 9999
 def getSearchSettings(mediaTitle):
     mediaTitle = mediaTitle.replace(".", " ")
