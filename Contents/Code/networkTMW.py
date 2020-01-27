91322b96fa9ae32b623d1c811c6573cc5df8d13f
Jani Grasten <janig88@gmail.com>
2020-01-27 14:25:30 +0000

    teenmegaworld-fix

--- Contents/Code/networkTMW.py
+++ Contents/Code/networkTMW.py
@@ -4,14 +4,14 @@ def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor
     if searchSiteID != 9999:
         siteNum = searchSiteID
     searchPageNum = 1
-    while searchPageNum <= 2:
+    while searchPageNum <= 5:
         searchResults = HTML.ElementFromURL(PAsearchSites.getSearchSearchURL(siteNum) + encodedTitle + "&page=" + str(searchPageNum))
         for searchResult in searchResults.xpath('//article'):
             if len(searchResult.xpath('//article')) > 0:
                 titleNoFormatting = searchResult.xpath('.//h1')[0].text_content().strip()
                 curID = searchResult.xpath('.//a')[0].get('href').replace('/','_').replace('?','!')
                 releaseDate = parse(searchResult.xpath('.//time')[0].text_content()).strftime('%Y-%m-%d')
-                subSite = searchResult.xpath('.//aside//div//a')[0].text_content().replace('.com','').strip()
+                subSite = searchResult.xpath('.//div[@class="site"]/a')[0].text_content().replace('.com','').strip()
                 if searchDate:
                     score = 100 - Util.LevenshteinDistance(searchDate, releaseDate)
                 else:
@@ -25,24 +25,24 @@ def update(metadata,siteID,movieGenres,movieActors):
     urlBase = PAsearchSites.getSearchBaseURL(siteID)
 
     # Title
-    metadata.title = detailsPageElements.xpath('//section[@class="video-block"]//h1')[0].text_content().strip()
+    metadata.title = detailsPageElements.xpath('//div[@class="title-line"]/h1')[0].text_content().strip()
     Log("Scene Title: " + metadata.title)
 
     # Studio/Tagline/Collection
     metadata.studio = "Teen Mega World"
-    subSite = detailsPageElements.xpath('//a[@class="site"]')[0].text_content().replace('.com','').strip()
+    subSite = detailsPageElements.xpath('//div[@class="site"]/a')[0].text_content().replace('.com','').strip()
     metadata.tagline = subSite
     metadata.collections.clear()
     metadata.collections.add(metadata.tagline)
 
     # Summary
     try:
-        metadata.summary = detailsPageElements.xpath('//p[@class="description"]')[0].text_content().strip()
+        metadata.summary = detailsPageElements.xpath('//div[@class="text"]//p')[0].text_content().strip()
     except:
         Log('No summary found')
 
     # Date
-    date = detailsPageElements.xpath('//section[@class="video-block"]//time')[0].text_content().strip()
+    date = detailsPageElements.xpath('//div[@class="date"]//time')[0].text_content().strip()
     Log("date: " + date)
     date_object = parse(date)
     metadata.originally_available_at = date_object
@@ -50,7 +50,7 @@ def update(metadata,siteID,movieGenres,movieActors):
 
     # Actors
     movieActors.clearActors()
-    actors = detailsPageElements.xpath('//ul[@class="girls"]//a')
+    actors = detailsPageElements.xpath('//div[@class="video"]//div[@class="site"]//a[position()>1]')
     Log("actors #: " + str(len(actors)))
     if len(actors) > 0:
         for actorLink in actors:
@@ -58,38 +58,16 @@ def update(metadata,siteID,movieGenres,movieActors):
             Log("Actor: " + actorName)
             actorPageURL = actorLink.get('href').replace('https','http')
             actorPageElements = HTML.ElementFromURL(actorPageURL)
-            actorPhotoURL = urlBase + actorPageElements.xpath('//div[@class="information"]//img')[0].get("src").replace('https','http')
+            actorPhotoURL = urlBase + actorPageElements.xpath('//div[@class="photo"]//img')[0].get("data-src").replace('https','http')
             Log("ActorPhotoURL: " + actorPhotoURL)
             movieActors.addActor(actorName,actorPhotoURL)
 
     # Genres
     movieGenres.clearGenres()
-    genres = detailsPageElements.xpath('//ul[@class="tags"]//a')
+    genres = detailsPageElements.xpath('//ul[@class="tag-list"]//a')
     if len(genres) > 0:
         for genreLink in genres:
             genre = genreLink.text_content()
             movieGenres.addGenre(genre)
-
-    # Posters/Background
-    valid_names = list()
-    metadata.posters.validate_keys(valid_names)
-    metadata.art.validate_keys(valid_names)
-
-    try:
-        background = urlBase + detailsPageElements.xpath('//div[@class="media-wrapper"]//video')[0].get("poster").replace('https','http')
-    except:
-        background = urlBase + detailsPageElements.xpath('//div[@class="media-wrapper"]//dl8-video')[0].get("poster").replace('https','http')
-
-    metadata.art[background] = Proxy.Preview(HTTP.Request(background, headers={'Referer': 'http://www.google.com'}).content, sort_order = 1)
-    metadata.posters[background] = Proxy.Preview(HTTP.Request(background, headers={'Referer': 'http://www.google.com'}).content, sort_order = 1)
-    Log("BG URL: " + background)
-
-    posters = detailsPageElements.xpath('//section[@class="photo-list"]//img')
-    Log(str(len(posters)) + " thumbs found.")
-    posterNum = 2
-    for posterCur in posters:
-        posterURL = urlBase + posterCur.get("src").replace('https','http')
-        metadata.posters[posterURL] = Proxy.Preview(HTTP.Request(posterURL, headers={'Referer': 'http://www.google.com'}).content, sort_order = posterNum)
-        posterNum += 1
 
     return metadata
