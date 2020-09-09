4343cd94f3df6fb4d24368af0f181bad6f2c46d6
DirtyRacer1337 <DirtyRacer@ya.ru>
2020-09-09 17:16:26 +0700

    Code Review

--- Contents/Code/networkDirtyHardDrive.py
+++ Contents/Code/networkDirtyHardDrive.py
@@ -3,107 +3,123 @@ import PAgenres
 import PAactors
 import PAutils
 
+
 def search(results, encodedTitle, searchTitle, siteNum, lang, searchDate):
-    url = PAsearchSites.getSearchSearchURL(siteNum)
-    req = PAutils.HTTPRequest(url)
-    sitemapElements = HTML.ElementFromString(req.text)
+    searchResults = []
+    googleResults = PAutils.getFromGoogleSearch(searchTitle, siteNum)
+    for sceneURL in googleResults:
+        if ('/tour1/' in sceneURL and sceneURL.endswith('.html') and sceneURL not in searchResults):
+            searchResults.append(sceneURL)
 
-    sceneResults = sitemapElements.xpath('//loc[contains(text(), "/tour1/")][contains(text(), ".html")]')
-    Log('Found ' + str(len(sceneResults)) + ' results.')
+    for sceneURL in searchResults:
+        req = PAutils.HTTPRequest(sceneURL)
+        detailsPageElements = HTML.ElementFromString(req.text)
 
-    for result in sceneResults:
-        filename = os.path.splitext(os.path.basename(result.text_content()))[0]
-        
-        title = filename.replace('_', ' ').title()
+        curID = PAutils.Encode(sceneURL)
+        titleNoFormatting = detailsPageElements.xpath('//h1')[0].text_content().strip()
+        releaseDate = searchDate if searchDate else ''
 
-        curID = PAutils.Encode(filename)
+        score = 100 - Util.LevenshteinDistance(searchTitle.lower(), titleNoFormatting.lower())
 
-        score = 100 - Util.LevenshteinDistance(searchTitle.lower(), title.lower())
-
-        if score > 80:
-            results.Append(MetadataSearchResult(id='%s|%d' % (curID, siteNum), name='%s [%s]' % (title, 'Dirty Hard Drive'), score=score, lang=lang))
+        results.Append(MetadataSearchResult(id='%s|%d|%s' % (curID, siteNum, releaseDate), name='%s [%s]' % (titleNoFormatting, PAsearchSites.getSearchSiteName(siteNum)), score=score, lang=lang))
 
     return results
 
+
 def update(metadata, siteID, movieGenres, movieActors):
     metadata_id = str(metadata.id).split('|')
-    sceneURL = 'http://bangmyhand.com/tour1/' + PAutils.Decode(metadata_id[0]) + '.html'
+    sceneURL = PAutils.Decode(metadata_id[0])
+
+    if len(metadata_id) > 2:
+        sceneDate = metadata_id[2]
 
     req = PAutils.HTTPRequest(sceneURL)
     detailsPageElements = HTML.ElementFromString(req.text)
 
     m = re.search("'playlistfile': '(.+playlist\.xml)'", req.text)
-    playListUrl = ''
-    tagline = ''
     if m:
         playListUrl = m.group(1)
-        Log('Playlist file found.')
-
-        if '/dhd_' in playListUrl:
-            tagline = 'Dirty Sluts and Studs'
-        elif '/bmh_' in playListUrl:
-            tagline = 'Bang My Hand'
-        elif '/kk_' in playListUrl:
-            tagline = 'Katie Kox'
-        elif '/lb_' in playListUrl:
-            tagline = 'Lee Bang XXX'
-        elif '/sdaf_' in playListUrl:
-            tagline = 'Sophie Dee and Friends'
-        elif '/kkng_' in playListUrl:
-            tagline = 'Kiera King'
-        elif '/gin_' in playListUrl:
-            tagline = 'Girls in Nylon'
 
         xmlPlaylistElements = XML.ElementFromURL(playListUrl)
-        poster = xmlPlaylistElements.xpath('//channel/item/*[name()="media:group"]/*[name()="media:thumbnail"]')[0].get('url')
+        poster = xmlPlaylistElements.xpath('//channel/item/*[name()="media:group"]/*[name()="media:thumbnail"]/@url')
+        if poster:
+            posterURL = poster[0]
     else:
         Log('Playlist file NOT found.')
         m = re.search("'image': '(.+bookend\.jpg)'", req.text)
         if m:
-            poster = m.group(1)
-    
-    if 'The Squirt Instructor' in req.text:
-        tagline = 'The Squirt Instructor'
+            posterURL = m.group(1)
 
-    if tagline == '': # Default
-        tagline = 'Dirty Sluts and Studs'
-    
     # Title
     metadata.title = detailsPageElements.xpath('//h1')[0].text_content().strip()
-    
+
     # Summary
-    metadata.summary = detailsPageElements.xpath('.//div[@id="video-page-desc"]')[0].text_content()
-    
+    metadata.summary = detailsPageElements.xpath('//div[@id="video-page-desc"]')[0].text_content().strip()
+
+    # Studio
     metadata.studio = 'Dirty Hard Drive'
 
     # Tagline and Collection(s)
     metadata.collections.clear()
+    tagline = PAsearchSites.getSearchSiteName(siteID)
     metadata.tagline = tagline
     metadata.collections.add(tagline)
 
     # Actors
     movieActors.clearActors()
-    for actorLink in detailsPageElements.xpath('//a[starts-with(@href, "pornstar_")]'):
-        actorName = actorLink.get('href')[9:-5].replace('_', ' ').title()
+    actorLink = detailsPageElements.xpath('//div[@id="video-specs"]//span')
+    if actorLink:
+        actorName = actorLink[-1].text_content().strip()
+        actorPhotoURL = ''
 
-        actorPageURL = actorLink.get('href')
-        req = PAutils.HTTPRequest('http://bangmyhand.com/tour1/' + actorPageURL)
-        actorPage = HTML.ElementFromString(req.text)
-        actorPhotoURL = actorPage.xpath('//div[@id="global-model-img"]//img/@src')[0]
+        if not actorName:
+            actorPageURL = actorLink.get('href')
+            actorName = actorPageURL.rsplit('/', 2)[-1].replace('.html', '', 1).replace('pornstar_', '', 1).replace('_', ' ').strip().title()
+            if actorPageURL.startswith('http'):
+                actorPageURL = PAsearchSites.getSearchBaseURL(siteID)
+
+            req = PAutils.HTTPRequest(actorPageURL)
+            actorPage = HTML.ElementFromString(req.text)
+            actorPhoto = actorPage.xpath('//div[@id="global-model-img"]//img/@src')
+            if actorPhoto:
+                actorPhotoURL = actorPhoto[0]
 
         movieActors.addActor(actorName, actorPhotoURL)
-    
-    # Posters/Background
-    valid_names = list()
-    metadata.posters.validate_keys(valid_names)
-    metadata.art.validate_keys(valid_names)
-    
-    posters = []
-    posters.append(poster)
 
-    posterNum = 1
-    for posterUrl in posters:
-        metadata.posters[posterUrl] = Proxy.Preview(HTTP.Request(posterUrl).content, sort_order = posterNum)
-        posterNum = posterNum + 1
+    # Release Date
+    if sceneDate:
+        date_object = parse(sceneDate)
+        metadata.originally_available_at = date_object
+        metadata.year = metadata.originally_available_at.year
+
+    # Posters/Background
+    art = []
+    xpaths = []
+
+    if posterURL:
+        art.append(posterURL)
+
+    for xpath in xpaths:
+        for img in detailsPageElements.xpath(xpath):
+            art.append(img)
+
+    Log('Artwork found: %d' % len(art))
+    for idx, posterUrl in enumerate(art, 1):
+        if not PAsearchSites.posterAlreadyExists(posterUrl, metadata):
+            # Download image file for analysis
+            try:
+                image = PAutils.HTTPRequest(posterUrl, headers={'Referer': 'http://www.google.com'})
+                im = StringIO(image.content)
+                resized_image = Image.open(im)
+                width, height = resized_image.size
+                # Add the image proxy items to the collection
+                if width > 1:
+                    # Item is a poster
+                    metadata.posters[posterUrl] = Proxy.Media(image.content, sort_order=idx)
+                if width > 100 and width > height:
+                    # Item is an art item
+                    metadata.art[posterUrl] = Proxy.Media(image.content, sort_order=idx)
+            except:
+                pass
 
     return metadata
--- Contents/Code/siteMeloneChallenge.py
+++ Contents/Code/siteMeloneChallenge.py
@@ -5,63 +5,43 @@ import PAutils
 
 
 def search(results, encodedTitle, searchTitle, siteNum, lang, searchDate):
+    searchResults = []
+    googleResults = PAutils.getFromGoogleSearch(searchTitle, siteNum)
+    for sceneURL in googleResults:
+        if ('/video/' in sceneURL and sceneURL not in searchResults):
+            searchResults.append(sceneURL)
 
-    pageNum = 1
-    while pageNum > 0:
-        req = PAutils.HTTPRequest(PAsearchSites.getSearchSearchURL(siteNum) + str(pageNum))
-        indexPageResults = HTML.ElementFromString(req.text)
+    for sceneURL in searchResults:
+        req = PAutils.HTTPRequest(sceneURL)
+        detailsPageElements = HTML.ElementFromString(req.text)
 
-        videos = indexPageResults.xpath('//div[@class="item"]')
-        Log.Info('Parsing %d results' % len(videos))
+        curID = PAutils.Encode(sceneURL)
+        titleNoFormatting = detailsPageElements.xpath('.//a[@class="dark"]')[0].text_content().strip()
+        releaseDate = searchDate if searchDate else ''
 
-        for video in videos:
-            titleLink = video.xpath('.//a[@class="dark"]')[0]
+        score = 100 - Util.LevenshteinDistance(searchTitle.lower(), titleNoFormatting.lower())
 
-            title = str(titleLink.text_content().strip())
-
-            m = re.search("/video/(\d+)/([a-z0-9\-]+)", titleLink.get('href'))
-            if m:
-                videoTitle = m.group(2)
-                videoId = m.group(1)
-                curID = PAutils.Encode(videoTitle + '-mea-melone-' + videoId)
-
-                score = 100
-                score = score - String.LevenshteinDistance(searchTitle.lower(), title.lower())
-
-                Log.Info('Found: %s / %d', title, score)
-                
-                if score > 95:
-                    results.Append(
-                        MetadataSearchResult(
-                            id = '%s|%d' % (curID, siteNum),
-                            name = '%s [%s]' % (title, 'Melone Challenge'),
-                            score = score,
-                            lang = lang
-                        )
-                    )
-
-                    return results
-
-        pageNum += 1
-        if len(indexPageResults.xpath('//a[text() = "page ' + str(pageNum) + '"]')) == 0:
-            Log.Debug('No more pages.')
-            pageNum = -1
+        results.Append(MetadataSearchResult(id='%s|%d|%s' % (curID, siteNum, releaseDate), name='%s [%s]' % (titleNoFormatting, PAsearchSites.getSearchSiteName(siteNum)), score=score, lang=lang))
 
     return results
 
+
 def update(metadata, siteID, movieGenres, movieActors):
+    metadata_id = str(metadata.id).split('|')
+    sceneURL = PAutils.Decode(metadata_id[0])
+    if sceneURL.startswith('http'):
+        PAsearchSites.getSearchBaseURL(siteID) + sceneURL
 
-    metadata_id = str(metadata.id).split('|')[0]
+    if len(metadata_id) > 2:
+        sceneDate = metadata_id[2]
 
-    req = PAutils.HTTPRequest(PAsearchSites.getSearchBaseURL(siteID) + PAutils.Decode(metadata_id))
-    detailsPageElement = HTML.ElementFromString(req.text)
+    req = PAutils.HTTPRequest(sceneURL)
+    detailsPageElements = HTML.ElementFromString(req.text)
 
     # Title
-    metadata.title = detailsPageElement.xpath('.//a[@class="dark"]')[0].text_content().strip()
-    
-    # Summary
-    metadata.summary = detailsPageElement.xpath('//p[@style="line-height:130%; font-size:13px"]')[0].text_content().strip()
+    metadata.title = detailsPageElements.xpath('.//a[@class="dark"]')[0].text_content().strip()
 
+    # Studio
     metadata.studio = 'Melone Challenge'
 
     # Tagline and Collection(s)
@@ -70,17 +50,39 @@ def update(metadata, siteID, movieGenres, movieActors):
     metadata.tagline = tagline
     metadata.collections.add(tagline)
 
-    # Posters/Background
-    valid_names = list()
-    metadata.posters.validate_keys(valid_names)
-    metadata.art.validate_keys(valid_names)
+    # Release Date
+    if sceneDate:
+        date_object = parse(sceneDate)
+        metadata.originally_available_at = date_object
+        metadata.year = metadata.originally_available_at.year
 
-    posters = [ detailsPageElement.xpath('.//figure/img/@src')[0] ]
-    
-    posterNum = 1
-    for posterUrl in posters:
-        image = PAutils.HTTPRequest(posterUrl, headers={'Referer': 'http://www.google.com'})
-        metadata.posters[posterUrl] = Proxy.Preview(image.content, sort_order = posterNum)
-        posterNum = posterNum + 1
+    # Posters/Background
+    art = []
+    xpaths = [
+        './/figure/img/@src'
+    ]
+
+    for xpath in xpaths:
+        for img in detailsPageElements.xpath(xpath):
+            art.append(img)
+
+    Log('Artwork found: %d' % len(art))
+    for idx, posterUrl in enumerate(art, 1):
+        if not PAsearchSites.posterAlreadyExists(posterUrl, metadata):
+            # Download image file for analysis
+            try:
+                image = PAutils.HTTPRequest(posterUrl, headers={'Referer': 'http://www.google.com'})
+                im = StringIO(image.content)
+                resized_image = Image.open(im)
+                width, height = resized_image.size
+                # Add the image proxy items to the collection
+                if width > 1:
+                    # Item is a poster
+                    metadata.posters[posterUrl] = Proxy.Media(image.content, sort_order=idx)
+                if width > 100 and width > height:
+                    # Item is an art item
+                    metadata.art[posterUrl] = Proxy.Media(image.content, sort_order=idx)
+            except:
+                pass
 
     return metadata
