1b8f14cdec0117d9528a54942525d8e17de12a2d
DirtyRacer1337 <DirtyRacer@ya.ru>
2020-09-15 02:29:17 +0700

    Update ActorSmartPlaylistGenerator.py

--- usefulscripts/Plex-ActorSmartPlaylistGenerator/ActorSmartPlaylistGenerator.py
+++ usefulscripts/Plex-ActorSmartPlaylistGenerator/ActorSmartPlaylistGenerator.py
@@ -2,26 +2,35 @@ from plexapi.server import PlexServer, Playlist
 
 BASEURL = ''  # Your Plex URL. If at localhost you can use 'http://127.0.0.1:port/'
 TOKEN = ''  # Your Plex Token. To find your token please follow this guide https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
-LIBRARY = ''  # Enter Here your title of your library. For example 'XXXMedia' if your library is named XXXMedia
+LIBRARIES = []  # Enter Here your titles of yours libraries. For example ['XXXMedia', 'XXXMedia 2'] if you're libraries is named XXXMedia and XXXMedia 2
 
 
 def create_playlist():
     plex = PlexServer(BASEURL, TOKEN)
-    plex_library = plex.library.section(LIBRARY)
-    plex_playlists = [playlist.title for playlist in plex.playlists()]
-    scenes = plex_library.all()
 
-    actors = []
-    for scene in scenes:
-        for actor in scene.actors:
-            actor_name = actor.tag
+    plex_scenes = {}
+    plex_playlists = {}
 
-            if actor_name and actor_name not in actors:
-                actors.append(actor_name)
+    for library_name in LIBRARIES:
+        plex_library = plex.library.section(library_name)
+        plex_playlists[plex_library] = [playlist for playlist in plex.playlists() if playlist.items()[0].section() == plex_library]
+        plex_scenes[plex_library] = plex_library.all()
 
-    for actor_name in actors:
-        if actor_name not in plex_playlists:
-            Playlist.create(plex, title=actor_name, section=LIBRARY, smart=True, actor=actor_name)
+    plex_actors = {}
+    for library_obj in plex_scenes:
+        plex_actors[library_obj] = []
+        for scene in plex_scenes[library_obj]:
+            for actor in scene.actors:
+                actor_name = actor.tag
+
+                if actor_name and actor_name not in plex_actors[library_obj]:
+                    plex_actors[library_obj].append(actor_name)
+
+    for library_obj in plex_scenes:
+        playlist_titles = [playlist.title for playlist in plex_playlists[library_obj]]
+        for actor_name in plex_actors[library_obj]:
+            if actor_name not in playlist_titles:
+                Playlist.create(plex, title=actor_name, section=library_obj, smart=True, actor=actor_name)
 
 if __name__ == '__main__':
     create_playlist()
