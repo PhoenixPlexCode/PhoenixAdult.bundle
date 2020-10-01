9c3cd5c306a8b650810016274a6a4d448c086ffd
Lazeraman <13284690+lazeraman@users.noreply.github.com>
2020-09-30 22:11:59 -0400

    Add Network TheScoreGroup
    Exact Matching Actor Name - SceneID
    
    Example: https://www.pornmegaload.com/hd-porn-scenes/Paige-Turner/55346
    
    Filename: PornMegaLoad - Paige Turner - 55346

---
+++ Contents/Code/networkScoreGroup.py
@@ -0,0 +1,99 @@
+import PAsearchSites
+import PAgenres
+import PAutils
+
+
+def search(results, encodedTitle, searchTitle, siteNum, lang, searchDate):
+    sceneID = re.sub('\D', '', searchTitle)
+    actorName = re.sub('\s\d.*', '', searchTitle).replace(' ', '-')
+    sceneURL = PAsearchSites.getSearchSearchURL(siteNum) + actorName + '/' + sceneID
+
+    req = PAutils.HTTPRequest(sceneURL)
+    detailsPageElements = HTML.ElementFromString(req.text)
+    titleNoFormatting = detailsPageElements.xpath('//h2')[1].text_content()
+
+    Log(titleNoFormatting)
+    curID = PAutils.Encode(sceneURL)
+
+    score = 100
+
+    results.Append(MetadataSearchResult(id='%s|%d' % (curID, siteNum), name='%s [%s]' % (titleNoFormatting, PAsearchSites.getSearchSiteName(siteNum)), score=score, lang=lang))
+
+    return results
+
+
+def update(metadata, siteID, movieGenres, movieActors):
+    metadata_id = str(metadata.id).split('|')
+    sceneURL = PAutils.Decode(metadata_id[0])
+    req = PAutils.HTTPRequest(sceneURL)
+    detailsPageElements = HTML.ElementFromString(req.text)
+
+    # Title
+    metadata.title = detailsPageElements.xpath('//h2')[1].text_content()
+
+    # Summary
+    summary = detailsPageElements.xpath('//div[@class="p-desc"]')[0].text_content().split('  ')
+    metadata.summary = summary[0]+summary[1]
+
+    # Studio
+    metadata.studio = 'Score Group'
+
+    # Tagline and Collection(s)
+    metadata.collections.clear()
+    metadata.tagline = PAsearchSites.getSearchSiteName(siteID)
+    metadata.collections.add(metadata.tagline)
+
+    # Release Date
+    date = detailsPageElements.xpath('//div/span[@class="value"]')[1].text_content()
+    date_object = parse(date)
+    metadata.originally_available_at = date_object
+    metadata.year = metadata.originally_available_at.year
+
+    # Actors
+    movieActors.clearActors()
+    for actor in detailsPageElements.xpath('//div/span[@class="value"]/a'):
+        actorName = actor.text_content().strip()
+        Log(actorName)
+        actorPhotoURL = ''
+
+        movieActors.addActor(actorName, actorPhotoURL)
+
+    # Genres
+    movieGenres.clearGenres()
+    for genreLink in detailsPageElements.xpath('//div[@class="mb-3"]/a'):
+        genreName = genreLink.text_content().strip()
+
+        movieGenres.addGenre(genreName)
+
+    # Posters/Background
+    art = []
+    xpaths = [
+        '//div/img/@src',
+    ]
+    for xpath in xpaths:
+        for poster in detailsPageElements.xpath(xpath):
+            if 'http' not in poster:
+                poster = 'http:' + poster
+            if 'shared-bits' not in poster:
+                art.append(poster)
+
+    Log('Artwork found: %d' % len(art))
+    for idx, posterUrl in enumerate(art, 1):
+        if not PAsearchSites.posterAlreadyExists(posterUrl, metadata):
+            # Download image file for analysis
+            try:
+                image = PAutils.HTTPRequest(posterUrl, headers={'Referer': 'http://www.google.com'})
+                im = StringIO(image.content)
+                resized_image = Image.open(im)
+                width, height = resized_image.size
+                # Add the image proxy items to the collection
+                if width > 1:
+                    # Item is a poster
+                    metadata.posters[posterUrl] = Proxy.Media(image.content, sort_order=idx)
+                if width > 100:
+                    # Item is an art item
+                    metadata.art[posterUrl] = Proxy.Media(image.content, sort_order=idx)
+            except:
+                pass
+
+    return metadata
