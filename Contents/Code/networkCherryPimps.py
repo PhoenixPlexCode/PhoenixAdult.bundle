be68c465a938c5ba112c96e735f2eb943a4789f9
DirtyRacer1337 <DirtyRacer@ya.ru>
2020-11-10 04:03:13 +0700

    Update networkCherryPimps.py

--- Contents/Code/networkCherryPimps.py
+++ Contents/Code/networkCherryPimps.py
@@ -9,16 +9,16 @@ def search(results, encodedTitle, searchTitle, siteNum, lang, searchDate):
         url = PAsearchSites.getSearchSearchURL(siteNum) + '%s&page=%d' % (searchTitle.replace(' ', '+'), searchPageNum)
         req = PAutils.HTTPRequest(url)
         searchResults = HTML.ElementFromString(req.text)
-        for searchResult in searchResults.xpath('//div[contains(@class, "video-thumb")]'):
-            titleNoFormatting = searchResult.xpath('.//p[@class="text-thumb"]/a')[0].text_content().strip()
-            curID = PAutils.Encode(searchResult.xpath('.//p[@class="text-thumb"]/a/@href')[0])
-            subSite = searchResult.xpath('.//p[@class="text-thumb"]//a[@class="badge"]')[0].text_content().strip()
+        for searchResult in searchResults.xpath('//div[contains(@class, "video-thumb") or contains(@class, "item-thumb")]'):
+            titleNoFormatting = searchResult.xpath('(.//p[@class="text-thumb"] || .//div[@class="item-title"])/a')[0].text_content().strip()
+            curID = PAutils.Encode(searchResult.xpath('(.//p[@class="text-thumb"] || .//div[@class="item-title"])/a/@href')[0])
+            subSite = searchResult.xpath('.//p[@class="text-thumb"]/a[@class="badge"] || .//div[@class="item-sitename"]/a')[0].text_content().strip()
 
-            date = searchResult.xpath('.//span[@class="date"]')[0].text_content().split('|')[1].strip()
+            date = searchResult.xpath('.//span[@class="date"] || .//div[@class="item-date"]')[0].text_content().split('|')[-1].strip()
             releaseDate = parse(date).strftime('%Y-%m-%d')
 
             actorNames = []
-            for actorLink in searchResult.xpath('.//span[@class="category"]//a'):
+            for actorLink in searchResult.xpath('(.//span[@class="category"] || .//div[@class="item-models"])//a'):
                 actorName = actorLink.text_content().strip()
 
                 actorNames.append(actorName)
@@ -43,10 +43,10 @@ def update(metadata, siteID, movieGenres, movieActors):
     detailsPageElements = HTML.ElementFromString(req.text)
 
     # Title
-    metadata.title = detailsPageElements.xpath('//*[@class="trailer-block_title"]')[0].text_content().strip()
+    metadata.title = detailsPageElements.xpath('//*[@class="trailer-block_title"] || //h1')[0].text_content().strip()
 
     # Summary
-    metadata.summary = detailsPageElements.xpath('//div[@class="info-block"]//p[@class="text"]')[0].text_content().strip()
+    metadata.summary = detailsPageElements.xpath('//div[@class="info-block"]//p[@class="text"] || //div[@class="update-info-block"]//p')[0].text_content().strip()
 
     # Studio
     metadata.studio = 'Cherry Pimps'
@@ -58,7 +58,7 @@ def update(metadata, siteID, movieGenres, movieActors):
     metadata.collections.add(tagline)
 
     # Release Date
-    date = detailsPageElements.xpath('//div[@class="info-block_data"]//p[@class="text"]')[0].text_content().split('|')[0].replace('Added', '').strip()
+    date = detailsPageElements.xpath('//div[@class="info-block_data"]//p[@class="text"] || //div[@class="update-info-row"]')[0].text_content().split('|')[0].replace('Added', '').strip()
     if date:
         date_object = parse(date)
         metadata.originally_available_at = date_object
@@ -66,14 +66,14 @@ def update(metadata, siteID, movieGenres, movieActors):
 
     # Genres
     movieGenres.clearGenres()
-    for genreLink in detailsPageElements.xpath('//div[@class="info-block"]//a'):
+    for genreLink in detailsPageElements.xpath('//div[@class="info-block"]//a || //ul[@class="tags"]//a'):
         genreName = genreLink.text_content().strip()
 
         movieGenres.addGenre(genreName)
 
     # Actors
     movieActors.clearActors()
-    actors = detailsPageElements.xpath('//div[@class="info-block_data"]//a')
+    actors = detailsPageElements.xpath('//div[@class="info-block_data"]//a || //div[contains(@class, "model-list-item")]//a')
     if actors:
         if len(actors) == 3:
             movieGenres.addGenre('Threesome')
@@ -84,23 +84,29 @@ def update(metadata, siteID, movieGenres, movieActors):
 
         for actorLink in actors:
             actorName = actorLink.text_content().strip()
+            if not actorName:
+                actorName = actorLink.xpath('//span')[0].text_content().strip()
             actorPhotoURL = ''
 
-            actorPageURL = actorLink.get('href')
-            if not actorPageURL.startswith('http'):
-                actorPageURL = PAsearchSites.getSearchBaseURL(siteID) + '/' + actorPageURL
+            actorPhoto = actorLink.xpath('//img/@src0_1x')
+            if actorPhoto:
+                actorPhotoURL = actorPhoto[0]
+            else:
+                actorPageURL = actorLink.get('href')
+                if not actorPageURL.startswith('http'):
+                    actorPageURL = PAsearchSites.getSearchBaseURL(siteID) + '/' + actorPageURL
 
-            req = PAutils.HTTPRequest(actorPageURL)
-            actorPage = HTML.ElementFromString(req.text)
+                req = PAutils.HTTPRequest(actorPageURL)
+                actorPage = HTML.ElementFromString(req.text)
 
-            img = actorPage.xpath('//img[contains(@class, "model_bio_thumb")]/@src')
-            if not img:
-                img = actorPage.xpath('//img[contains(@class, "model_bio_thumb")]@src0_1x')
+                img = actorPage.xpath('//img[contains(@class, "model_bio_thumb")]/@src')
+                if not img:
+                    img = actorPage.xpath('//img[contains(@class, "model_bio_thumb")]@src0_1x')
 
-            if img:
-                actorPhotoURL = img[0]
-                if not actorPhotoURL.startswith('http'):
-                    actorPhotoURL = 'https:' + actorPhotoURL
+                if img:
+                    actorPhotoURL = img[0]
+                    if not actorPhotoURL.startswith('http'):
+                        actorPhotoURL = 'https:' + actorPhotoURL
 
             movieActors.addActor(actorName, actorPhotoURL)
 
