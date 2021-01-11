5442d10223b1e59d3826db24abedfc1e4bf73873
DirtyRacer1337 <DirtyRacer@ya.ru>
2021-01-11 17:52:16 +0700

    Change encoding

--- Contents/Code/PAutils.py
+++ Contents/Code/PAutils.py
@@ -195,11 +195,11 @@ def saveRequest(url, req):
         os.makedirs(debug_dir)
 
     raw_http = '< Target URL: "%s"\r\n\r\n' % url
-    raw_http += unicode(dump.dump_all(req))
+    raw_http += dump.dump_all(req).decode('UTF-8', errors='replace')
 
     file_name = '%s.gz' % uuid.uuid4().hex
     with gzip.open(debug_dir + file_name, 'wb') as f:
-        f.write(raw_http)
+        f.write(raw_http.encode('UTF-8'))
 
     Log('GZip request saved as "%s"' % file_name)
 
