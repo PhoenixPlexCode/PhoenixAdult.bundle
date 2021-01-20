0068a1224565845fc606711786c6ca9241f62eb5
DirtyRacer1337 <DirtyRacer@ya.ru>
2021-01-21 04:28:38 +0700

    Update networkSteppedUp.py

--- Contents/Code/networkSteppedUp.py
+++ Contents/Code/networkSteppedUp.py
@@ -1,11 +1,10 @@
 import PAsearchSites
 import PAutils
-import re
 
 
 def search(results, lang, siteNum, searchData):
     searchData.encoded = searchData.title.replace(' ', '-').replace('--', '-').replace('\'', '').lower()
-    if "/" not in encodedTitle and re.match("\d+.*",encodedTitle):
+    if '/' not in searchData.encoded and re.match(r'\d+.*', searchData.encoded):
         searchData.encoded = searchData.encoded.replace('-', '/', 1)
 
     sceneURL = PAsearchSites.getSearchSearchURL(siteNum) + searchData.encoded
