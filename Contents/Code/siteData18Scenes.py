9a1ab036244250fb4518e5eff8810cd6ca2ab27e
ConstrictM <87558008+ConstrictM@users.noreply.github.com>
2021-12-31 02:37:10 -0500

    Loop Through All Pages found in Search

--- Contents/Code/siteData18Scenes.py
+++ Contents/Code/siteData18Scenes.py
@@ -23,44 +23,56 @@ def search(results, lang, siteNum, searchData):
     req = PAutils.HTTPRequest(searchURL, headers={'Referer': 'https://www.data18.com'})
     searchPageElements = HTML.ElementFromString(req.text)
 
-    for searchResult in searchPageElements.xpath('//a'):
-        sceneURL = searchResult.xpath('./@href')[0]
+    searchPages = re.search(r'(?<=pages:\s).*(?=])', req.text)
+    if searchPages:
+        numSearchPages = int(searchPages.group(0))
+    else:
+        numSearchPages = 1
 
-        if ('/scenes/' in sceneURL and sceneURL not in searchResults):
-            urlID = re.sub(r'.*/', '', sceneURL)
+    for idx in range(0, numSearchPages):
+        for searchResult in searchPageElements.xpath('//a'):
+            sceneURL = searchResult.xpath('./@href')[0]
 
-            try:
-                siteDisplay = searchResult.xpath('.//i')[0].text_content().strip()
-            except:
-                siteDisplay = ''
+            if ('/scenes/' in sceneURL and sceneURL not in searchResults):
+                urlID = re.sub(r'.*/', '', sceneURL)
 
-            titleNoFormatting = PAutils.parseTitle(searchResult.xpath('.//p[@class="gen12 bold"]')[0].text_content(), siteNum)
-            curID = PAutils.Encode(sceneURL)
-            siteResults.append(sceneURL)
+                try:
+                    siteDisplay = searchResult.xpath('.//i')[0].text_content().strip()
+                except:
+                    siteDisplay = ''
 
-            try:
-                date = searchResult.xpath('.//span[@class="gen11"]/text()')[0].strip()
-            except:
-                date = ''
+                titleNoFormatting = PAutils.parseTitle(searchResult.xpath('.//p[@class="gen12 bold"]')[0].text_content(), siteNum)
+                curID = PAutils.Encode(sceneURL)
+                siteResults.append(sceneURL)
 
-            if date and not date == 'unknown':
-                releaseDate = datetime.strptime(date, "%B %d, %Y").strftime('%Y-%m-%d')
-            else:
-                releaseDate = searchData.dateFormat() if searchData.date else ''
-            displayDate = releaseDate if date else ''
+                try:
+                    date = searchResult.xpath('.//span[@class="gen11"]/text()')[0].strip()
+                except:
+                    date = ''
 
-            if sceneID == urlID:
-                score = 100
-            elif searchData.date and displayDate:
-                score = 80 - Util.LevenshteinDistance(searchData.date, releaseDate)
-            else:
-                score = 80 - Util.LevenshteinDistance(searchData.title.lower(), titleNoFormatting.lower())
+                if date and not date == 'unknown':
+                    releaseDate = datetime.strptime(date, "%B %d, %Y").strftime('%Y-%m-%d')
+                else:
+                    releaseDate = searchData.dateFormat() if searchData.date else ''
+                displayDate = releaseDate if date else ''
 
-            if score == 80:
-                count += 1
-                temp.append(MetadataSearchResult(id='%s|%d|%s' % (curID, siteNum, releaseDate), name='%s [%s] %s' % (titleNoFormatting, siteDisplay, displayDate), score=score, lang=lang))
-            else:
-                results.Append(MetadataSearchResult(id='%s|%d|%s' % (curID, siteNum, releaseDate), name='%s [%s] %s' % (titleNoFormatting, siteDisplay, displayDate), score=score, lang=lang))
+                if sceneID == urlID:
+                    score = 100
+                elif searchData.date and displayDate:
+                    score = 80 - Util.LevenshteinDistance(searchData.date, releaseDate)
+                else:
+                    score = 80 - Util.LevenshteinDistance(searchData.title.lower(), titleNoFormatting.lower())
+
+                if score == 80:
+                    count += 1
+                    temp.append(MetadataSearchResult(id='%s|%d|%s' % (curID, siteNum, releaseDate), name='%s [%s] %s' % (titleNoFormatting, siteDisplay, displayDate), score=score, lang=lang))
+                else:
+                    results.Append(MetadataSearchResult(id='%s|%d|%s' % (curID, siteNum, releaseDate), name='%s [%s] %s' % (titleNoFormatting, siteDisplay, displayDate), score=score, lang=lang))
+
+        if numSearchPages > 1 and not idx + 1 == numSearchPages:
+            searchURL = '%s%s&key2=%s&next=1&page=%d' % (PAsearchSites.getSearchSearchURL(siteNum), searchData.encoded, searchData.encoded, idx + 1)
+            req = PAutils.HTTPRequest(searchURL, headers={'Referer': 'https://www.data18.com'})
+            searchPageElements = HTML.ElementFromString(req.text)
 
     googleResults = PAutils.getFromGoogleSearch(searchData.title, siteNum)
     for sceneURL in googleResults:
