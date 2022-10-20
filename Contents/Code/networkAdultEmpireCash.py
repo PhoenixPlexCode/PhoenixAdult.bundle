9585c33aeb44b15ca5265a97ec603bf0e9fce4ce
DiamondPete <87245367+diamondpete@users.noreply.github.com>
2022-10-20 16:38:12 -0400

    Fix Date Match

--- Contents/Code/networkAdultEmpireCash.py
+++ Contents/Code/networkAdultEmpireCash.py
@@ -10,7 +10,7 @@ def search(results, lang, siteNum, searchData):
             # Modification for JAYs POV and SpankMonster
             titleNoFormatting = PAutils.parseTitle(searchResult.xpath('.//img[contains(@class, "img-full-fluid")]/@title')[0], siteNum)
             curID = PAutils.Encode(searchResult.xpath('.//article[contains(@class, "scene-update")]/a/@href')[0])
-            date = searchResult.xpath('//span[@class="date"]')
+            date = searchResult.xpath('.//span[@class="date"]')
             if date:
                 releaseDate = parse(date[0].text_content().strip()).strftime('%Y-%m-%d')
             else:
