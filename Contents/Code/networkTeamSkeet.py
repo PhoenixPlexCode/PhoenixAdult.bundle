062ff83da9b712c2a36a33c071f657c2392e2971
DiamondPete <87245367+diamondpete@users.noreply.github.com>
2022-10-25 12:35:15 -0400

    Revert Custom Scoring

--- Contents/Code/networkTeamSkeet.py
+++ Contents/Code/networkTeamSkeet.py
@@ -62,9 +62,9 @@ def search(results, lang, siteNum, searchData):
                 displayDate = releaseDate if 'publishedDate' in detailsPageElements else ''
 
                 if searchData.date and displayDate:
-                    score = 80 - Util.LevenshteinDistance(searchData.date, releaseDate)
+                    score = 100 - Util.LevenshteinDistance(searchData.date, releaseDate)
                 else:
-                    score = 80 - Util.LevenshteinDistance(searchData.title.lower(), titleNoFormatting.lower())
+                    score = 100 - Util.LevenshteinDistance(searchData.title.lower(), titleNoFormatting.lower())
 
                 results.Append(MetadataSearchResult(id='%s|%d|%s|%s' % (curID, siteNum, releaseDate, sceneType), name='%s [%s] %s' % (titleNoFormatting, subSite, displayDate), score=score, lang=lang))
 
