2e4455c613d69b1bb34d4ea869f3fcbbf36cf0ff
DiamondPete <87245367+diamondpete@users.noreply.github.com>
2023-01-22 23:04:47 -0500

    Fix Punctuation

--- Contents/Code/PAutils.py
+++ Contents/Code/PAutils.py
@@ -391,9 +391,9 @@ def postParseTitle(output):
     # Remove single period at end of title
     output = re.sub(r'(?<=[^\.].)(?<=\w)(?:\.)$', '', output)
     # Remove space between word and punctuation
-    output = re.sub(r'\s+(?=[.,!:])', '', output)
-    # Remove space between hashtag and word
-    output = re.sub(r'(?<=[#])\s+', '', output)
+    output = re.sub(r'\s+(?=[.,!:\'\)])', '', output)
+    # Remove space between punctuation and word
+    output = re.sub(r'(?<=[#\(])\s+', '', output)
     # Override lowercase if word follows a punctuation
     output = re.sub(r'(?<=!|:|\?|\.|-)(\s)(\S)', lambda m: m.group(1) + m.group(2).upper(), output)
     # Override lowercase if word follows a parenthesis
